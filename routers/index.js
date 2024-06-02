0ce978215eccd8206b000e41870309dba8cc576c
ravindrareddy12 <reddysomula6789@gmail.com>
2024-06-02 22:55:40 +0530

    login

--- routers/index.js
+++ routers/index.js
@@ -5,6 +5,7 @@ const proposalController = require('../Controllers/proposalController');
 
 router.post('/signup',userController.signup)
 router.post('/signin',userController.login)
+router.post('/login',userController.login)
 router.post('/pressReleases', proposalController.createPressRelease);
 router.get('/pressReleases', proposalController.getPressReleases);
 module.exports = router;
